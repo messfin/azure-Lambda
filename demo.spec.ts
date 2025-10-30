77116223bc3ce4b77c655c8827802851b1250da1
messfin <32806625+messfin@users.noreply.github.com>
2025-10-29 20:58:20 -0400

    Initial commit

---
+++ demo.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect, Page } from '@playwright/test';
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+});
+test.describe('Demo Test', () => {
+    test('Verify Login Error Message', async ({ page }) => {
+        await page.waitForSelector('#user-name',{state:'visible'});
+        await page.locator('[data-test="username"]').type('example1@example.com');
+        await page.locator('[data-test="password"]').type('examplepassword');
+        await page.locator('[data-test="login-button"]').click();
+        const errorMessage = await page.locator('[data-test="error"]').textContent();
+        console.log("Login Error Message: "+errorMessage);
+        expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
+        page.close();
+    });
+});
