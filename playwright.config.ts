ceed6866315a7d15a4d9d61820b1d6de0f90303a
Mesfin Assefa <mesfin@zmroute.com>
2025-11-04 01:43:02 +0000

    Updated playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Use 40 workers locally; 1 on CI */
-  workers: process.env.CI ? 1 : 40,
+  workers: process.env.CI ? 1 : 10,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
 
